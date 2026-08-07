05c24d00a7acd38d99c13390c644e2518a58cafb
J-Co-de <jclark3987@gmail.com>
2026-08-06 21:00:35 -0500

    I set up POSTCSS and I started work on the sign in card!

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const starBg = document.querySelector(".starry-background");
-const starCount = 800;
+const starCount = 1000;
 for (let i = 0; i < starCount; i++) {
   const star = document.createElement("div");
   const radius = Math.random() * 4 + "px";
   star.className = "star";
-  star.style.top = Math.random() * 100 + "vh";
-  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 98 + 1 + "vh";
+  star.style.left = Math.random() * 98 + 1 + "vw";
   star.style.width = radius;
   star.style.height = radius;
   star.style.opacity = Math.random();
